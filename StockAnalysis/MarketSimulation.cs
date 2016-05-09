aceaa4e98cdd08e175891d6bd51ecc11bc693dcd
Xecuter <sam.h.leclerc@gmail.com>
2016-05-09 18:44:56 -0400

    first commit

---
+++ StockAnalysis/MarketSimulation.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAnalysis
+{
+	using System.IO;
+	using System.Threading;
+
+	class MarketSimulation
+	{
+		public double Capital { get; set; }
+		public int Stock { get; set; }
+		public double PercentageThresholdToSell { get; set; }
+		public int MaxDaysOfHolding { get; set; }
+		public double DaysCounter { get; set; }
+		public double LastBuyPrice { get; set; }
+		public double MaxCapital { get; set; }
+		public int AttemptedTrades { get; set; }
+		public int DoneTrades { get; set; }
+		public int ReachedMaxTrades { get; set; }
+
+		StreamWriter sw;
+
+		public MarketSimulation() { }
+
+		public MarketSimulation(double capital, int stock, double percentageToSell, int maxDaysHolding = 0)
+		{
+			Capital = capital;
+			Stock = stock;
+			PercentageThresholdToSell = percentageToSell;
+			LastBuyPrice = 0;
+			DaysCounter = 0;
+			MaxCapital = 0;
+			AttemptedTrades = 0;
+			DoneTrades = 0;
+			ReachedMaxTrades = 0;
+
+			if (maxDaysHolding == 0) MaxDaysOfHolding = 999999;
+			else MaxDaysOfHolding = maxDaysHolding;
+
+			sw = new StreamWriter($"output/{percentageToSell}_{maxDaysHolding}.csv");
+			sw.WriteLine($"date,openingCapital,openingStock,Capital,Stock,LastBuyPrice,openingStockPrice,highStockPrice,closingStockPrice,highStockPrice/LastBuyPrice,DaysCounter");
+		}
+
+		public void NewTradingDay(string date, double openingStockPrice, double highStockPrice, double closingStockPrice)
+		{
+			double openingCapital = Capital;
+			double openingStock = Stock;
+
+			// if we don't have any stock, we buy some first
+			if (Stock <= 0)
+			{
+				BuyStock(openingStockPrice);
+			}
+
+			// we try to sell our stocks at the established margin
+			if ((highStockPrice / LastBuyPrice) >= PercentageThresholdToSell)
+			{
+				SellStock(LastBuyPrice * PercentageThresholdToSell);
+				DoneTrades++;
+			}
+			// we sell at closing price if we reached max days of holding
+			else if (DaysCounter >= MaxDaysOfHolding)
+			{
+				SellStock(closingStockPrice);
+				ReachedMaxTrades++;
+			}
+			// we did not sell today, we increase the counter
+			else
+			{
+				DaysCounter++;
+			}
+
+			AttemptedTrades++;
+
+			// write to file
+			sw.WriteLine($"{date},{openingCapital},{openingStock},{Capital},{Stock},{LastBuyPrice},{openingStockPrice},{highStockPrice},{closingStockPrice},{highStockPrice / LastBuyPrice},{DaysCounter}");
+		}
+
+		public void BuyStock(double stockPrice)
+		{
+			LastBuyPrice = stockPrice;
+			//Capital -= 10;
+			Stock = (int)Math.Floor(Capital / stockPrice);
+			Capital -= Stock * stockPrice;
+		}
+
+		public void SellStock(double stockPrice)
+		{
+			Capital += Stock * stockPrice;
+			//Capital -= 10;
+			Stock = 0;
+			DaysCounter = 0;
+
+			if (Capital > MaxCapital) MaxCapital = Capital;
+		}
+
+		public override string ToString()
+		{
+			return $"[MaxCapital: {MaxCapital}; Capital: {Capital}; Stock: {Stock}; DaysCounter: {DaysCounter}; LastBuy: {LastBuyPrice}; Threshold: {PercentageThresholdToSell}; MaxHolding: {MaxDaysOfHolding}]";
+        }
+	}
+}
---
+++ StockAnalysis/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAnalysis
+{
+	using System.IO;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			bool verbose = false;
+
+			var marketSimulators = new List<MarketSimulation>();
+
+			//var simulator = new MarketSimulation(30000, 0, 1.01, 15);
+			//marketSimulators.Add(simulator);
+
+			for (int iDays = 0; iDays <= 100; iDays++)
+			{
+				for (int iThres = 5; iThres <= 100; iThres += 5)
+				{
+					var simulator = new MarketSimulation(30000, 0, 1 + ((double)iThres / 10000), iDays);
+					marketSimulators.Add(simulator);
+				}
+			}
+
+			using (StreamReader sr = new StreamReader("csv/google1416.csv"))
+			{
+				// Discard file header
+				sr.ReadLine();
+
+				while (!sr.EndOfStream)
+				{
+					var data = sr.ReadLine().Split(new[] { ',' });
+					string date = data[0];
+					double openingPrice = double.Parse(data[1]);
+					double highPrice = double.Parse(data[2]);
+					double lowPrice = double.Parse(data[3]);
+					double closePrice = double.Parse(data[4]);
+
+					foreach (var marketSimulation in marketSimulators)
+					{
+						marketSimulation.NewTradingDay(date, openingPrice, highPrice, closePrice);
+					}
+				}
+			}
+
+			// Write summary file
+			using (StreamWriter sw = new StreamWriter("output/summary.csv"))
+			{
+				// Header
+				sw.WriteLine("MaxDaysHolding,Threshold,MaxCapital,AttemptedTrades,DoneTrades,MaxReachedTrades");
+				foreach (MarketSimulation marketSimulation in marketSimulators)
+				{
+					sw.WriteLine($"{marketSimulation.MaxDaysOfHolding},{marketSimulation.PercentageThresholdToSell},{marketSimulation.MaxCapital},{marketSimulation.AttemptedTrades},{marketSimulation.DoneTrades},{marketSimulation.ReachedMaxTrades}");
+				}
+			}
+
+			//Console.ReadKey();
+		}
+	}
+}
